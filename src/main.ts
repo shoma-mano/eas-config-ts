211f6c87b9c1d2afe4d5963a278c632e40ff6b59
shoma-mano <ms2geki@gmail.com>
2024-08-13 22:29:07 +0900

    remove empty fields from config

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,20 @@ const main = defineCommand({
     const config = jiti(join(projectDir, "eas.config.ts"));
 
     // remove empty fields
-    Object.keys(config).forEach((key) => {
-      if (!config[key]) {
-        delete config[key];
-      }
-    });
+    const removeEmptyFields = (obj: any) => {
+      Object.keys(obj).forEach((key) => {
+        if (obj[key] && typeof obj[key] === "object") {
+          removeEmptyFields(obj[key]);
+        } else if (
+          obj[key] === null ||
+          obj[key] === undefined ||
+          obj[key] === ""
+        ) {
+          delete obj[key];
+        }
+      });
+    };
+    removeEmptyFields(config);
 
     writeFileSync(
       join(projectDir, "eas.json"),
